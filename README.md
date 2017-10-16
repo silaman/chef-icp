50650c4da72d431c7f2498d993e7c7999cfe1dc6
ravi <ravi@zippy.ramvas.home>
2017-10-16 15:29:26 -0700

    recipe 10_worker_node works

--- README.md
+++ README.md
@@ -13,12 +13,14 @@ Chef makes node's NAT as the default "ipaddress". The custom attribute "chef_ip"
 
 `knife bootstrap 192.168.56.30 -x labrat -N icp-mbp --sudo --json-attributes '{ "chef_ip": "192.168.56.30" }' --use-sudo-password -P Obj#ct00`
 
-`knife bootstrap 192.168.56.32 -x labrat -N icp-work2 --sudo --json-attributes '{ "chef_ip": "192.168.56.32" }' --use-sudo-password -P Obj#ct00`
+`knife bootstrap 192.168.56.31 -x labrat -N icp-work1 --sudo --json-attributes '{ "chef_ip": "192.168.56.31" }' --use-sudo-password -P Obj#ct00`
 
 Add run list to node
 --------------------
 `knife node run_list set icp-mbp 'recipe[icp::default],recipe[icp::00_master_node]'`
 
-`knife node run_list set icp-work1 'recipe[icp::default],recipe[icp::10_work_proxy_node]'`
+`knife node run_list set icp-work1 'recipe[icp::default],recipe[icp::10_worker_node]'`
 
 `knife ssh 'name:icp-mbp' 'sudo chef-client' --ssh-user labrat --ssh-password 'Obj#ct00' --attribute chef_ip`
+
+`knife ssh 'name:icp-work1' 'sudo chef-client' --ssh-user labrat --ssh-password 'Obj#ct00' --attribute chef_ip`
---
+++ recipes/10_worker_node.rb
@@ -0,0 +1,74 @@
+#
+# Cookbook:: icp
+# Recipe:: os_parms
+#  ICP Workder Nodes
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Create .ssh folder for non-root & root accounts
+directory "#{ENV['HOME']}/.ssh" do
+  action :create
+end
+directory '/root/.ssh' do
+  action :create
+end
+
+ruby_block 'ssh_store_key' do
+  block do
+    # Add SSH KEY to non-root & root authorized_keys
+    def add_key_to_authorized_keys(key)
+      open(File.expand_path('~/.ssh/authorized_keys'), 'a') do |f|
+        if File.readlines(f).grep("#{key}").any?
+          Chef::Log.info("key is already in non-root authorized keys")
+        else
+          Chef::Log.info("Adding SSH KEY to non-root authorized keys")
+          f << key
+        end
+      end
+      open(File.expand_path('/root/.ssh/authorized_keys'), 'a') do |f|
+        if File.readlines(f).grep("#{key}").any?
+          Chef::Log.info("key is already in root authorized keys")
+        else
+          Chef::Log.info("Adding SSH KEY to root authorized keys")
+          f << key
+        end
+      end
+    end
+
+    master_pub_key = ""
+    loop_max_tries = 100
+    attempts = 1
+    # Get the ICP cluster's master_pub_key from the master_node. Place it in
+    # the current (worker or proxy) node's master_pub_key attribute, in
+    # /root/.ssh/authorized_keys and current user's ~/.ssh/authorized_keys
+    while attempts <= loop_max_tries
+      Chef::Log.info("attempts: #{attempts} - loop_max_tries: #{loop_max_tries}")
+      search(:node, 'icp_node_type:"master_node"') do |n|
+        master_pub_key = n['ibm']['icp_master_pub_key']
+      end
+
+      if !master_pub_key.to_s.empty?
+        Chef::Log.info("-- master_pub_key #{master_pub_key}")
+        break
+      end
+
+      sleep 10
+      attempts += 1
+    end
+
+    if master_pub_key.to_s.empty?
+      raise "EXITING: Cannot determine master_pub_key"
+    end
+
+    sleep 20
+
+    add_key_to_authorized_keys(master_pub_key)
+    node.normal['ibm']['icp_master_pub_key'] = master_pub_key
+    icp_node_type = "worker_node"
+    node.normal['ibm']['icp_node_type'] = icp_node_type
+    node.save
+    Chef::Log.info("icp_node_type: #{'icp_node_type'}")
+    Chef::Log.info("node.ibm.icp_node_type: #{node['ibm']['icp_node_type']}")
+
+  end
+
+end
