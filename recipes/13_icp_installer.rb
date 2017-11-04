218b1388e6bcfe1a79bd65f21fb899e3b287c72e
Ravi Ramnarayan <ravi.ramnarayan@gmail.com>
2017-11-04 15:13:45 -0700

    13_icp_installer uses icp_cluster data bag items to compose cluster/hosts

--- recipes/13_icp_installer.rb
+++ recipes/13_icp_installer.rb
@@ -53,18 +53,40 @@ docker_container 'icp_extract_installer' do
   tag         icp['tag']
   env         icp['env']
   command     icp['command']
-  working_dir '/opt/ibm-cloud-private-ce'
-  volumes     '/opt/ibm-cloud-private-ce:/data'
-  not_if { ::File.exist?(::File.expand_path("/opt/ibm-cloud-private-ce/cluster/hosts")) }
+  working_dir '/opt/ibm-cloud-private'
+  volumes     '/opt/ibm-cloud-private:/data'
+  not_if { ::File.exist?(::File.expand_path("/opt/ibm-cloud-private/cluster/hosts")) }
 end
 
 # Replace icp-ce cluster/hosts file with fixed values in the template. Need to
 # make the template flexible -- later.
-template '/opt/ibm-cloud-private-ce/cluster/hosts' do
-  source 'icpce_cluster_hosts.erb'
+master_nodes = ["[master]"]
+worker_nodes = ["[worker]"]
+proxy_nodes = ["[proxy]"]
+mgmt_nodes = ["[management]"]
+
+data_bag('icp_cluster').each do |icp_node|
+  nd = data_bag_item('icp_cluster', icp_node)
+  # Add IP addresses to node type arrays
+  if nd['icp_node_type'].to_s == "master"
+    master_nodes = master_nodes + [ nd['ip_address'] ]
+  elsif nd['icp_node_type'].to_s == "worker"
+    worker_nodes = worker_nodes + [ nd['ip_address'] ]
+  elsif nd['icp_node_type'].to_s == "proxy"
+    proxy_nodes = proxy_nodes + [ nd['ip_address'] ]
+  elsif nd['icp_node_type'].to_s == "management"
+    mgmt_nodes = mgmt_nodes + [ nd['ip_address'] ]
+  end
+end
+
+master_nodes = master_nodes + worker_nodes + proxy_nodes + mgmt_nodes
+
+template '/opt/ibm-cloud-private/cluster/hosts' do
+  source 'icp_cluster_hosts.erb'
+  variables( :cluster_hosts => master_nodes )
 end
 
 # Copy master.id_rsa to ICPce installer
 execute 'copy ssh_key' do
-  command 'sudo cp ~/.ssh/master.id_rsa /opt/ibm-cloud-private-ce/cluster/ssh_key'
+  command 'sudo cp ~/.ssh/master.id_rsa /opt/ibm-cloud-private/cluster/ssh_key'
 end
